cce37c88c920f221d4b5aa62b26be2a26e6a0c00
John Lapeyre <jlapeyre@users.noreply.github.com>
2021-11-24 15:14:09 -0500

    Import module in test instead of using
    This allows runtest.jl to be used in PackageCompiler along with
    other packages that also export symbol Pauli.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,8 @@
-using QiskitQuantumInfo
+import QiskitQuantumInfo
 using Test
 
 @testset "QiskitQuantumInfo.jl" begin
     for s in ("X", "Y")
-        @test string(Pauli(s)) == s
+        @test string(QiskitQuantumInfo.Pauli(s)) == s
     end
 end
