3bf68b428129c243545e77e7346755cce5cc8955
John Lapeyre <jlapeyre@users.noreply.github.com>
2021-11-25 13:44:30 -0500

    missing new file:   src/distance.jl

---
+++ src/distance.jl
@@ -0,0 +1,19 @@
+function hellinger_distance!(p_counts::AbstractDict{T,V}, q_counts::AbstractDict{T,V}) where {T, V}
+    p_sum = sum(values(p_counts))
+    q_sum = sum(values(q_counts))
+    total = float(zero(V))
+    for (key, val) in p_counts
+        if haskey(q_counts, key)
+            total += (sqrt(val / p_sum) - sqrt(q_counts[key] / q_sum))^2
+            delete!(q_counts, key)
+        else
+            total += val / p_sum
+        end
+    end
+    total += sum(x -> x / q_sum, values(q_counts))
+    dist = sqrt(total / 2)
+    return dist
+end
+
+hellinger_distance(p_counts::AbstractDict, q_counts::AbstractDict) =
+    hellinger_distance!(copy(p_counts), copy(q_counts))
